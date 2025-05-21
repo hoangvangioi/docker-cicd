5237f72366921427c3d94d9512d569cd3ba45572
Hoang Van Gioi <gioitube2k2@gmail.com>
2025-05-21 10:21:04 +0700

    update cicd docker 3

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello from Go Web App!",
+			"message": "Hello from Go Web App on Docker!",
 		})
 	})
 
